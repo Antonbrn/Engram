a7bc50c75b5b56e2ee5d196d917bc3c674e30e83
Mattias Åkerlund <mattiasakerlund@live.se>
2020-05-26 11:14:33 +0200

    Gjort en theme med färger

---
+++ photo-app/src/components/theme.js
@@ -0,0 +1,8 @@
+//Orangea fäger
+export const carrotOrange = "#f57f17";
+export const koromiko = "#ffb04c";
+export const tenneTowny = "#bc5100";
+//Gröna färger
+export const emerald = "#54c969";
+export const mintGreen = "#89fd98";
+export const salem = "#0E973C";
