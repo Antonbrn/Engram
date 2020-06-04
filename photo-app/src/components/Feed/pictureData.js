e0e016de77d03570a1f9937df7e378196aa13ef3
Mattias Akerlund <mattiasakerlund@live.se>
2020-06-04 11:19:58 +0200

    fixat feed, hårdkodad bild så länge

---
+++ photo-app/src/components/Feed/pictureData.js
@@ -0,0 +1,31 @@
+import image from "./lulea.png";
+
+const pictureData = [{
+    img: image,
+    title: 'Titel',
+    author: 'Anton',
+    featured: true,
+},
+{
+     img: image,
+     title: 'Lulea',
+     author: 'Mattias',
+     featured: false,
+    
+},
+{
+    img: image,
+    title: 'Min stad',
+    author: 'Daniel',
+    featured: false,
+   
+},
+{
+    img: image,
+    title: 'Ketchup?',
+    author: 'Agust',
+    featured: true,
+   
+}];
+
+export default pictureData;
