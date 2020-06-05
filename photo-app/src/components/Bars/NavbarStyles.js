179f28c9e1c2ffb708378a821610d604b8a0451e
Alex <alexandersundstrom1@hotmail.com>
2020-06-05 11:11:29 +0200

    123

--- photo-app/src/components/Bars/NavbarStyles.js
+++ photo-app/src/components/Bars/NavbarStyles.js
@@ -1,1 +1,23 @@
 import styled from "styled-components";
+import { Typography, Box, Container, Button } from "@material-ui/core";
+
+
+export const StyledBox = styled(Box)`
+
+`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default {};
