1e9e02078aba5852718832b3eb536bb66387383f
Antonbrn <anton.brannstrom@cmeducations.com>
2020-06-09 15:28:02 +0200

    fixed so that login doesn't render on update

--- photo-app/src/Auth.js
+++ photo-app/src/Auth.js
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from "react";
 import fire from "./base.js";
 import { db } from "./base.js";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 export const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
+  const [pending, setPending] = useState(true);
 
   useEffect(() => {
-    fire.auth().onAuthStateChanged(setCurrentUser);
+    fire.auth().onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setPending(false);
+    });
     fire.auth().onAuthStateChanged((user) => {
       db.collection("users")
         .doc(user.uid)
@@ -20,6 +25,11 @@ export const AuthProvider = ({ children }) => {
         });
     });
   }, []);
+
+  if (pending) {
+    return <CircularProgress />;
+  }
+
   return (
     <AuthContext.Provider value={{ currentUser }}>
       {children}
