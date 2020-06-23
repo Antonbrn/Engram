b0671f8937ae11b7b25cb1f414e58a6a7d023d4d
Antonbrn <anton.brannstrom@cmeducations.com>
2020-06-23 14:15:05 +0200

    error fixing

--- photo-app/src/components/ProfilePage/ProfilePage.js
+++ photo-app/src/components/ProfilePage/ProfilePage.js
@@ -40,25 +40,21 @@ const ProfilePage = () => {
           </Link>
         </TypographyProfile>
 
-      <EmailButton href="mailto:Agust.Ronnback@cmeducations.se?&subject=Feedback&body=We%20apreciate%20you%20taking%20time%20to%20send%20us%20your%20feedback">
-        Send us your Feedback
-      </EmailButton>
-
-        <EmailButton
-          a
-          href="mailto:Agust.Ronnback@cmeducations.se?&subject=Feedback&body=We%20apreciate%20you%20taking%20time%20to%20send%20us%20your%20feedback"
-        >
+        <EmailButton href="mailto:Agust.Ronnback@cmeducations.se?&subject=Feedback&body=We%20apreciate%20you%20taking%20time%20to%20send%20us%20your%20feedback">
           Send us your Feedback
-      </EmailButton>
-      <div>
-      <ButtonStyled
-        onClick={() => fire.auth().signOut().then(window.location.reload())}
-      >
-        Log out
-      </ButtonStyled>
-      </div>
-      </ProfileDivContainer>
+        </EmailButton>
 
+        <EmailButton href="mailto:Agust.Ronnback@cmeducations.se?&subject=Feedback&body=We%20apreciate%20you%20taking%20time%20to%20send%20us%20your%20feedback">
+          Send us your Feedback
+        </EmailButton>
+        <div>
+          <ButtonStyled
+            onClick={() => fire.auth().signOut().then(window.location.reload())}
+          >
+            Log out
+          </ButtonStyled>
+        </div>
+      </ProfileDivContainer>
     </BoxContainer>
   );
 };
